31b46c8e022693a0b741cb27bd2eccc33032d075
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2023-06-02 18:08:45 +0200

    update tests

--- src/Tests/MainTest.test.tsx
+++ src/Tests/MainTest.test.tsx
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom'
 import { render, screen } from "@testing-library/react";
+import user from '@testing-library/user-event';
 import App from "../App";
 import Inputs from '../Containers/Inputs/Inputs';
 
@@ -12,7 +13,10 @@ describe(`Trial tests group`, () => {
         expect(text).toBeInTheDocument();
     })
 
-    test(`GetByRole`, () => {
+    test(`GetByRole`, async () => {
+
+        user.setup();
+
         render(<Inputs
             day={``}
             setDay={() => { }}
@@ -24,17 +28,19 @@ describe(`Trial tests group`, () => {
             monthError=''
             yearError=''
         />);
-        const inputElement = screen.getAllByRole(`textbox`);
+        const inputElement = screen.getAllByRole(`textbox`); // moze byc regex
 
         expect(inputElement[0]).toBeInTheDocument();
+
+        await user.click(inputEmelent);
+
+        const inputElementPoClikniecu = screen.getAllByRole(`textbox`); // moze byc regex
+        expect(co co zmienilo sie po klikniecu);
+
     })
 
 })
 
-//  getBy..  //  queryBy..  //  findBy..  //
-//  getAllBy..  //  queryAllBy..  // findAllBy..  //
-//  getBy..-> Role (Accessible Rich Internet Applications[link,button,checkBox...{ można dodać własna role}]),
-//            LabelText, PlaceHolderText, Text, DisplayValue, AltText, Title, TestId
 
 
 // Przykładowe Testy
@@ -48,6 +54,59 @@ describe(`Trial tests group`, () => {
 // kod trzeciej strony
 // kod nie potrzebny z punktu widzenia urzytkownika np. nie testowac jakiejs funkcji do wyswieltania tylko to
 //          czy cos sie wyswietliow na stronie
+
+
+
+
+
+//  getBy..  //  queryBy..  //  findBy..  //
+//  getAllBy..  //  queryAllBy..  // findAllBy..  //
+//  getBy..-> Role (Accessible Rich Internet Applications[link,button,checkBox...{ można dodać własna role}]),
+//     |       LabelText,
+//     |       PlaceHolderText,
+//     |       Text,
+//     |       DisplayValue,
+//     |       AltText,
+//     |       Title, -> cos co pojawia sie gdy najezdzasz myszka
+//     |/      TestId -> data-testid="id do testu"
+
+
+// query -> zwraca node query albo null jesli nie znajdzie ale nie wyrzuci bład. Przydatne w sytulacjach w ktorych
+//          spodziewaly sie ze elemetu nie bedzie.
+
+
+// find -> Do testow elemtow async. Zwraca obietnice ktora jest wykonywana gdy znajdzie element
+//         'await screen.findBY(...{timeout:3000})
+
+
+
+// Biblioteka  'uesr-event' (jest instalowany domyślnie). Instnieje fireEvents z RTL ale nie jest tak dobry.
+// click()
+// dblClick()
+// tripleClick()
+// hover
+// unhover
+
+// type(element,`cos`) -> toHaveValue
+// tab                 -> toHaveFocus
+// keyboard(`opcje`)
+
+// clear
+// selectOptions/deselectOptions
+// uplaod 
+// copy
+// cut
+// paste
+
+// pointer({keys:`[MouseLeft]`}) - wiecej opcjie w dokumentaci ( przy opuszczaniu przycisu itp.)
+
+
+
+
+
+
+
+// screen.debug() ->  pokazuje gdzie w html wystepuje bład
 
 
 
